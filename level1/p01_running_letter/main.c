36dfba4ccadd0fad338ce1d08bc1ac25afcfbfd0
SCP-Infty <1993853393@qq.com>
2024-09-25 20:27:22 +0800

    p01 p02 submitted

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,44 @@
-#include <printf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
 int main() {
-    printf("hello world!\n");
+
+    int count = 0; // 空格数
+    int max = 30; // 边界
+    int stop = 50; // ͣ停顿时间
+    int rev = 0; // 反转
+    while (1) {
+        system("cls");
+
+        for (int i = 0; i < count; i++) {
+            printf(" ");
+        }
+
+        printf("0");
+
+        for (int i = 0; i < max-count; i++) {
+            printf(" ");
+        }
+
+        printf("|\n");
+
+        Sleep(stop);
+
+        if (rev) {
+            count--;
+        }
+        else {
+            count++;
+        }
+
+        if (count == max) {
+            rev = 1;
+        }
+        else if(count == 0) {
+            rev = 0;
+        }
+
+    }
     return 0;
 }
--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,32 @@
-#include <printf.h>
+#include<stdio.h>
+#include<math.h>
 
 int main() {
-    printf("hello world!\n");
+    unsigned int inputNumber;
+    short is_prime;
+
+    while (1) {
+        printf("Please input a positive integer: ");
+        scanf("%d", &inputNumber);
+
+        if (inputNumber < 3) {
+            printf("Invalid input.\n\n");
+            continue;
+        }
+
+        is_prime = 1;
+        for (int i = 2; i <= sqrt(inputNumber); i++) {
+            if (inputNumber % i == 0) {
+                is_prime = 0;
+                break;
+            }
+        }
+
+        if (is_prime) {
+            printf("Yes, it's a prime number!\n\n");
+            continue;
+        }
+        printf("No, it's not a prime number!\n\n");
+    }
     return 0;
 }
